fa9b9824e6af608d8db38d1f5203a9c36bf3c03d
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-12-15 20:41:14 +0100

    add table users in db, add new user into database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import dao.UserDao;
+import models.User;
+
 public class Main {
     public static void main(String[] args) {
-
+        User user = new User("Kamil", "Kamil@email.pl",
+                "password");
+        UserDao userDao = new UserDao();
+        userDao.create(user);
     }
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,18 +1,38 @@
 package dao;
 
 import DBConnection.DBConnection;
+import models.User;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+
 
 public class UserDao {
-     Connection dbConnection;
+    private static final String CREATE_USER_QUERY = "INSERT INTO users(username, email, password) VALUES (?,?,?)";
+    private static final String READ_USER_QUERY = "SELECT * FROM users WHERE id = ?";
+    private static final String UPDATE_USER_QUERY = "UPDATE users SET username = ?, emial = ?, password = ? WHERE id = ?";
+    private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE id = ?";
+    private static final String FIND_ALL_USERS_QUERY = "SELECT * FROM users?";
 
-    {
+
+
+
+    public User create(User user) {
         try {
-            dbConnection = new DBConnection().getConnection();
+            Connection connection = new DBConnection().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, user.getUserName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getPassword());
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+
+            if(resultSet.next()){
+                user.setId(resultSet.getInt(1));
+                return user;
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return null;
     }
 }
