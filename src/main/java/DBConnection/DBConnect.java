f66f2a6ce94fb297f0e7147cd8367d7949b515ad
Przemysław Kośmider <kosmiderprzemyslaw91@gmail.com>
2020-12-15 07:15:56 +0100

    configure maven project, add new class

---
+++ src/main/java/DBConnection/DBConnect.java
@@ -0,0 +1,8 @@
+package DBConnection;
+
+public class DBConnect {
+    private final String URL =
+            "jdbc:mysql://localhost:3306/jdbc_test?useSSL=false&characterEncoding=utf8";
+    private final String USER = "root";
+    private final String PASSWORD = "coderslab";
+}
---
+++ src/main/java/models/User.java
@@ -0,0 +1,56 @@
+package models;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class User {
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.hashPassword(password);
+    }
+
+    private void hashPassword(String password) {
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
